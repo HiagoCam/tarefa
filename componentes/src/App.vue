690ddc747ff3e60a517af1da11be0bbfa3e3fb91
HiagoCam <hiago_campregher@estudante.sc.senai.br>
2020-04-12 23:00:35 -0300

    tarefa concluida

---
+++ componentes/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <TodoForm @addTarefa="adicionarTarefa($event)"/>
+    <TodoList :tarefas="tarefas"/>
+  </div>
+</template>
+
+<script>
+import TodoForm from "./components/TodoForm.vue"
+import TodoList from "./components/TodoList.vue"
+
+export default {
+  name: 'App',
+  components: {
+    TodoForm, TodoList
+  }, data:function(){
+    return{
+        tarefas:[
+        {descricao:"Matemática"},
+        {descricao:"Biologia"}
+      ]
+    }
+  },
+  methods:{
+    adicionarTarefa: function(event){
+      this.tarefas.push({descricao: event.novaTarefa})
+    }
+   }
+}
+</script>
+
+<style>
+</style>
